92846b523a1008f7451a37112466b347e106e990
Vince van Oosten <vince@vince.lol>
2016-12-23 12:14:29 +0100

    clean-up main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,17 +4,9 @@ use maillurgy::socket;
 
 use maillurgy::smtp::server::server as smtp_server;
 
-#[macro_use]
-extern crate nom;
-
 
 fn main() {
 
   let p = socket::start(smtp_server);
   println!("main end: {:?}", p);
 }
-
-// fn main() {
-//
-    // socket::start(smtp_server);
-// }
