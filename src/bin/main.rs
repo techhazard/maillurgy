37d3caf1d4c7a976db021433bca5e76c7942f71c
Vince v. Oosten <awesomefireduck@gmail.com>
2016-12-30 10:07:53 +0100

    add tests for src/socket and src/bin/main (#5)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,5 +9,37 @@ use maillurgy::smtp::server::server as smtp_server;
 fn main() {
 
   let p = socket::start(smtp_server);
-  dbugln!("main end: {:?}", p);
+}
+
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_main() {
+
+        use std::thread;
+        use std::net::SocketAddr;
+        use std::str::FromStr;
+        use std::time::Duration;
+        use std::net::TcpStream;
+        use std::io::Write;
+        let millis = Duration::from_millis(50);
+
+
+        let testing_socket = SocketAddr::from_str("127.0.0.1:1337").unwrap();
+
+        let start_thread = thread::spawn(move || {
+            super::main();
+        });
+
+        // wait some time until the server exists (50ms)
+        thread::park_timeout(millis);
+
+        let mut stream = TcpStream::connect(testing_socket).unwrap();
+        dbugln!("{:?}",stream);
+
+        let _ = stream.write(&b"QUIT\r\n"[..]);
+        dbugln!("quit sent");
+
+    }
 }
--- src/socket.rs
+++ src/socket.rs
@@ -7,8 +7,10 @@ use std::io::Error;
 /// stops working or when hardware stops working
 ///
 /// requires a function that accepts a tcpstream
-pub fn start<F: Fn(&TcpStream)>(handle_stream: F) -> Result<(), Error> {
-    let listener = TcpListener::bind("127.0.0.1:1337")?;
+pub fn start<F: Fn(&TcpStream)>(handle_stream: F) {
+
+    // TODO: error handling
+    let listener : TcpListener = TcpListener::bind("127.0.0.1:1337").unwrap();
 
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
@@ -19,5 +21,40 @@ pub fn start<F: Fn(&TcpStream)>(handle_stream: F) -> Result<(), Error> {
             Err(_) => { /* connection failed */ }
         }
     }
-    Ok(())
+    ()
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_start() {
+
+        use std::thread;
+        use std::net::SocketAddr;
+        use std::str::FromStr;
+        use std::time::Duration;
+        use std::net::TcpStream;
+        use std::io::Write;
+        let millis = Duration::from_millis(50);
+
+
+        let testing_socket = SocketAddr::from_str("127.0.0.1:1337").unwrap();
+
+        let start_thread = thread::spawn(move || {
+            fn handle_stream(a: &TcpStream) {}
+            dbugln!("starting server");
+            super::start(handle_stream);
+        });
+
+        // wait some time until the server exists (50ms)
+        thread::park_timeout(millis);
+
+        let mut stream = TcpStream::connect(testing_socket).unwrap();
+        dbugln!("{:?}",stream);
+
+        let _ = stream.write(&b"QUIT\r\n"[..]);
+        dbugln!("quit sent");
+
+    dbugln!("server quit");
+    }
 }
